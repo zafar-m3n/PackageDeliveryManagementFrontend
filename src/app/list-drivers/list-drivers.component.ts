b59c1ca5c24a10932143ced7b2bb1a6ad6a01d9a
zafar-m3n <thezafar.m3n@gmail.com>
2024-10-12 16:38:49 +0530

    Changes

--- src/app/list-drivers/list-drivers.component.ts
+++ src/app/list-drivers/list-drivers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DriverService, Driver } from '../services/driver.service';
+import { PackageService, Package } from '../services/package.service';
 import { CustomUppercasePipe } from '../upper-case-pipe.pipe';
 
 @Component({
@@ -13,20 +14,28 @@ import { CustomUppercasePipe } from '../upper-case-pipe.pipe';
 })
 export class ListDriversComponent implements OnInit {
   drivers: Driver[] = [];
+  packages: Package[] = [];
+  filteredPackages: Package[] = [];
   loading: boolean = true;
   error: string | null = null;
+  selectedDriverId: string | null = null;
+  selectedDriverName: string | null = null;
 
-  constructor(private driverService: DriverService) {}
+  constructor(
+    private driverService: DriverService,
+    private packageService: PackageService
+  ) {}
 
   ngOnInit() {
-    this.fetchDrivers();
+    this.fetchData();
   }
 
-  fetchDrivers() {
+  fetchData() {
+    // Fetch drivers and packages simultaneously
     this.driverService.getDrivers().subscribe({
       next: (data) => {
         this.drivers = data;
-        this.loading = false;
+        this.checkIfDataIsLoaded();
       },
       error: (err) => {
         console.error('Error fetching drivers:', err);
@@ -34,5 +43,31 @@ export class ListDriversComponent implements OnInit {
         this.loading = false;
       },
     });
+
+    this.packageService.getPackages().subscribe({
+      next: (data) => {
+        this.packages = data;
+        this.checkIfDataIsLoaded();
+      },
+      error: (err) => {
+        console.error('Error fetching packages:', err);
+        this.error = 'Failed to load packages. Please try again later.';
+        this.loading = false;
+      },
+    });
+  }
+
+  checkIfDataIsLoaded() {
+    if (this.drivers.length > 0 && this.packages.length > 0) {
+      this.loading = false;
+    }
+  }
+
+  filterPackagesByDriver(driver_id: string, driver_name: string) {
+    this.selectedDriverId = driver_id;
+    this.selectedDriverName = driver_name; // Capture the driver's name
+    this.filteredPackages = this.packages.filter(
+      (pkg) => pkg.driver_id === driver_id
+    );
   }
 }
